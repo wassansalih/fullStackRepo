1803957282282469f5e69d2049de0d718d149d2f
wsulaima <wsulaima@jaguarlandrover.com>
2020-06-23 16:21:06 +0100

    back-end: authorize the request before response

--- backEnd/src/controllers/index.js
+++ backEnd/src/controllers/index.js
@@ -1,12 +1,23 @@
 const data = require("../data").data
+const { token } = require("../data/mySecretData")
 
 // eslint-disable-next-line no-unused-vars
 exports.getTime = (req, res, next) => {
-
-	res.setHeader("Content-Type", "application/json")
-	res.status(200).json({
-		timeData: data.timeData
-	})
+    const auth = req.headers.authorization
+    if(auth !== token) {
+        res.setHeader("Content-Type", "application/json")
+        res.status(403).json({
+            error: {
+                message: "UnAuthorized user",
+                code:403
+            }
+        })
+    } else {
+        res.setHeader("Content-Type", "application/json")
+        res.status(200).json({
+            timeData: data.timeData
+        })
+    }
 }
 
 exports.getMetrics= (req, res, next) => {
