c61c0a941ca6a017a14be1c3f529acfe4b87e9b7
wsulaima <wsulaima@jaguarlandrover.com>
2020-06-23 17:38:28 +0100

    back-end: add prometheus middleware

--- backEnd/src/router/routes.js
+++ backEnd/src/router/routes.js
@@ -3,6 +3,5 @@ let router = express.Router();
 const controller = require("../controllers")
 
 router.get("/time", controller.getTime)
-router.get("/metrics", controller.getMetrics)
 
 module.exports = router
