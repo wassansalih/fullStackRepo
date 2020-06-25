323a51df6910a29a64b895f413f0f8f51d17f9f4
wsulaima <wsulaima@jaguarlandrover.com>
2020-06-25 12:24:29 +0100

    front-end: render changed machine time every sec and refetch server time every 30

--- front-end/src/test/TimeComponent.test.js
+++ front-end/src/test/TimeComponent.test.js
@@ -1,35 +1,42 @@
-import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
-import EnzymeAsapter from 'enzyme-adapter-react-16'
-import Time from '../components/TimeComponent'
+import React from 'react';
+import Enzyme, { shallow, mount } from 'enzyme';
+import EnzymeAsapter from 'enzyme-adapter-react-16';
+import Time from '../components/TimeComponent';
 
 Enzyme.configure({ adapter: new EnzymeAsapter });
 
 describe("Time component testing", () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = shallow(<Time />)
+    wrapper = shallow(<Time/>)
   });
-  test('renders main div element ', () => {
+  test('renders title', () => {
     const element = wrapper.find(`[data-test="time-component-title"]`)
     expect(element.length).toBe(1)
     expect(element.text()).toContain('TIME COMPONENT')
   });
-  test ('renders main div element ', () => {
+  test ('renders  server-time', () => {
     const element = wrapper.find(`[data-test="time-component-server-time"]`)
     expect(element.length).toBe(1)
-    expect(element.text()).toContain('Server time')
-  });
-  test('renders main div element ', () => {
-    const element = wrapper.find(`[data-test="time-component-machine-time"]`)
-    expect(element.length).toBe(1)
-    expect(element.text()).toContain('Machine time')
+    expect(element.text()).toContain('Server time in seconds:')
   });
 
-  test('renders main div element ', () => {
+  test('renders difference-time ', () => {
     const element = wrapper.find(`[data-test="time-component-difference-time"]`)
     expect(element.length).toBe(1)
     expect(element.text()).toContain('The difference between machine and server times:')
+  });
+  it('should call msToTime function in render`', () => {
+    const instance = wrapper.instance();
+    jest.spyOn(instance, 'msToTime');
+    instance.render();
+    expect(instance.msToTime).toHaveBeenCalledTimes(1);
+  });
+
+  it('msToTime should return formated time as string`', () => {
+    const instance = wrapper.instance();
+    const formatedTime = instance.msToTime(1593083676725);
+    expect(formatedTime).toBe('11:14:36.7');
   });
 
 })
